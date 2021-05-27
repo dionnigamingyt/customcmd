404277dda00d4986c7ca9b64c06a9c8492fd588b
dionnigamingyt <82177465+dionnigamingyt@users.noreply.github.com>
2021-05-27 23:11:05 +0200

    E
    The change log and bat file

---
+++ 2.cmd
@@ -0,0 +1,156 @@
+:startup
+title Loading
+echo Loading...
+echo.
+echo Please Wait...
+echo.
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+title Starting
+echo Starting...
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+cls
+:app
+title "Your Title"
+set /p "a=Tutorial Command: "
+if ["%a%"] == ["clear"] goto clear
+if ["%a%"] == ["test_app"] goto test_app
+if ["%a%"] == ["clear"] goto clear
+
+cls
+echo Error: The command you tried starting is invalid
+echo.
+pause >nul
+cls
+goto app
+
+
+:clear
+cls
+goto :app
+
+:test_app
+cls
+echo Starting Operation...
+@echo off
+:startup
+title Loading
+echo Loading...
+echo.
+echo Please Wait...
+echo.
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+title Starting
+echo Starting...
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+cls
+:app
+title "Your Title"
+set /p "a=Tutorial Command: "
+if ["%a%"] == ["clear"] goto clear
+if ["%a%"] == ["test_app"] goto test
+if ["%a%"] == ["clear"] goto clear
+
+:clear
+cls
+goto :app
+
+:test
+cls
+echo Starting Operation...
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+ping -n 3 >nul
+echo Test has been made.
+pause >nul
+goto app
